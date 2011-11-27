3f53ff7948dfc49880f50a60e7ac26636c57df21
shivprasad@gmail.com <shivprasad@gmail.com@6aa95435-0683-8d4b-a703-06d58eaf18b0>
2011-11-27 16:18:17 +0000

    initial commit

---
+++ src/byteco/de/intellij/plugin/eclipsechangelistaction/ChangelistActionConfiguration.java
@@ -0,0 +1,67 @@
+package byteco.de.intellij.plugin.eclipsechangelistaction;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChangelistActionConfiguration {
+
+	private JTextField command;
+	private JTextField commandArgs;
+	private JPanel panel;
+	private JCheckBox consoleOutput;
+	private JCheckBox executeInBackground;
+    private JButton toolPathSelector;
+    private JButton configSelector;
+
+    public ChangelistActionConfiguration() {
+        toolPathSelector.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                JFileChooser fc = new JFileChooser();
+                int returnVal = fc.showOpenDialog(panel);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                command.setText(fc.getSelectedFile().getAbsolutePath());
+            }
+            }
+        });
+        configSelector.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                JFileChooser fc = new JFileChooser();
+                int returnVal = fc.showOpenDialog(panel);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                commandArgs.setText(fc.getSelectedFile().getAbsolutePath());
+            }
+            }
+        });
+    }
+
+    public JPanel getPanel() {
+		return panel;
+	}
+
+	private ChangelistActionComponent.State state = new ChangelistActionComponent.State();
+
+	public void load(ChangelistActionComponent.State state) {
+		this.state = state;
+        command.setText(state.command);
+        commandArgs.setText(state.commandArgs);
+        consoleOutput.setSelected(state.consoleOutput);
+        executeInBackground.setSelected(state.executeInBackground);
+	}
+
+	public ChangelistActionComponent.State save() {
+		state.command = command.getText();
+		state.commandArgs = commandArgs.getText();
+		state.consoleOutput = consoleOutput.isSelected();
+		state.executeInBackground = executeInBackground.isSelected();
+		return state;
+	}
+
+	public boolean isModified() {
+		return !state.command.equals(command.getText()) ||
+			!state.commandArgs.equals(commandArgs.getText()) ||
+			state.consoleOutput != consoleOutput.isSelected() ||
+			state.executeInBackground != executeInBackground.isSelected()
+		;
+	}
+}
